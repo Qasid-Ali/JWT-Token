732b000a0a9956e3762251151a2bbd741aab014c
Qasid-Ali <qasidali856@gmail.com>
2024-01-31 08:30:41 +0500

    Added cors error

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 require("dotenv").config();
 const express = require("express");
 const jwt = require("jsonwebtoken");
+const cors = require('cors');
+
 const secretKey = "secretKey";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(cors());
 
 app.get("/", (req, res) => {
     res.json({
